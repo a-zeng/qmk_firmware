b54a5a2237aa28aae390ab2c8c299b710d19a98e
Alex Zeng <azeng68@gmail.com>
2024-03-17 16:38:16 -0400

    Fix RGBLED_NUM configuration

--- keyboards/keebio/viterbi/keymaps/azure/config.h
+++ keyboards/keebio/viterbi/keymaps/azure/config.h
@@ -17,11 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-<<<<<<< HEAD:keyboards/keebio/viterbi/keymaps/azure/config.h
 // #define USE_I2C
-=======
-// place overrides here
->>>>>>> 1c1bc565dea0b2a8f7ccafc2d002b119b9f4e0fd:keyboards/rgbkb/zen/rev1/keymaps/jwlawrence/config.h
 
 /* Select hand configuration */
 
@@ -29,24 +25,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #undef RGBLED_NUM
-<<<<<<< HEAD:keyboards/keebio/viterbi/keymaps/azure/config.h
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_SPLIT
 #define RGBLIGHT_LAYERS
 #define RGBLED_NUM 14
-=======
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#define RGBLED_NUM 16
->>>>>>> 1c1bc565dea0b2a8f7ccafc2d002b119b9f4e0fd:keyboards/rgbkb/zen/rev1/keymaps/jwlawrence/config.h
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
